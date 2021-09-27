cf7e031bea38617e3ccff137f940d5421506e74c
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2021-09-27 11:43:36 +0700

    responsive test #1 | add Attribution | Deploy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
+import Footer from 'layout/Footer'
 import Hero from 'layout/Hero'
 import Register from 'layout/Register'
 import React from 'react'
 
 export default function Home() {
     return (
+        <>
         <main>
             <Hero />
             <Register />
         </main>
+        <Footer />
+        </>
     )
 }
